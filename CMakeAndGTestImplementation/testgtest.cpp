27f4201c9372a2d8acea75757e9771df609e207e
jonathan-rizk <jonrizk@protonmail.com>
2020-02-28 20:35:53 -0600

    cmake and gtest

---
+++ CMakeAndGTestImplementation/testgtest.cpp
@@ -0,0 +1,14 @@
+// #include <iostream>
+
+#include "gtest\include\gtest\gtest.h"
+
+TEST(sample_test_case, sample_test)
+{
+    EXPECT_EQ(1, 1);
+}
+// using namespace std;
+// int main()
+// {
+//     cout<<"HELLO WORLD"<<endl;
+//     return 0;
+// }
